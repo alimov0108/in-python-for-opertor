9859ad37cceacf8d28f32683ba9187635cb65eb0
unknown <maruf0108.1@g,ail.com>
2023-02-04 17:41:19 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+son = int(input("son: "))
+
+if son % 2 == 0:
+    print("juft son")
+    for x in range(son):
+        print(x)
+
+elif son % 2 != 0:
+    print("toq son")
+    for x in range(son):
+        print(x)
